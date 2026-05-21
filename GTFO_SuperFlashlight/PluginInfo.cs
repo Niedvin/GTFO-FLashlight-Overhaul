70932d22429a66bb72d347dcdd9d8a25ca4ce617
Niedvin <108703904+Niedvin@users.noreply.github.com>
2026-05-21 11:56:17 +0300

    Delete GTFO_SuperFlashlight directory

--- GTFO_SuperFlashlight/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace GTFO_SuperFlashlight
-{
-    internal static class PluginInfo
-    {
-        public const string GUID    = "FlashlightOverhaul";
-        public const string Name    = "Flashlight Overhaul";
-        public const string Version = "1.0.0";
-    }
-}
